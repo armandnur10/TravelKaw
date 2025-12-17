09e9ffcce8e73b55801ecea4d55873a25260dc9d
Gipariii-T100 <ghivaribaihaqi@gmail.com>
2025-12-17 20:51:49 +0700

    Update Gambar Destinasi

--- js/destinasi.js
+++ js/destinasi.js
@@ -4,7 +4,7 @@ const destinations = [
         name: 'Raja Ampat, Papua',
         location: 'West Papua',
         category: 'alam',
-        image: 'https://images.unsplash.com/photo-1559827260-dc66d52bef19?w=800',
+        image: 'https://images.unsplash.com/photo-1703769605297-cc74106244d9?q=80&w=884',
         description: 'Underwater paradise with stunning coral reefs and rich marine life',
         popular: true
     },
@@ -13,7 +13,7 @@ const destinations = [
         name: 'Candi Borobudur',
         location: 'Magelang, Central Java',
         category: 'budaya',
-        image: 'https://images.unsplash.com/photo-1559827260-dc66d52bef19?w=800',
+        image: 'https://images.unsplash.com/photo-1705905343745-6d901a93e946?q=80&w=449',
         description: 'The largest Buddhist temple in the world and a UNESCO World Heritage site',
         popular: true
     },
@@ -22,7 +22,7 @@ const destinations = [
         name: 'Pantai Kuta, Bali',
         location: 'Badung, Bali',
         category: 'alam',
-        image: 'https://images.unsplash.com/photo-1559827260-dc66d52bef19?w=800',
+        image: 'https://images.unsplash.com/photo-1666873536534-7b1a5a333d24?q=80&w=910',
         description: 'An exotic beach famous for its breathtaking sunset views',
         popular: true
     },
@@ -31,7 +31,7 @@ const destinations = [
         name: 'Candi Prambanan',
         location: 'Sleman, Yogyakarta',
         category: 'budaya',
-        image: 'https://images.unsplash.com/photo-1559827260-dc66d52bef19?w=800',
+        image: 'https://images.unsplash.com/photo-1576233475048-55f08b53a8bd?q=80&w=387',
         description: 'The largest Hindu temple complex in Indonesia',
         popular: true
     },
@@ -40,7 +40,7 @@ const destinations = [
         name: 'Gunung Bromo',
         location: 'Probolinggo, East Java',
         category: 'alam',
-        image: 'https://images.unsplash.com/photo-1559827260-dc66d52bef19?w=800',
+        image: 'https://plus.unsplash.com/premium_photo-1697729935951-420138024919?q=80&w=774',
         description: 'An active volcano with spectacular sunrise panoramas',
         popular: false
     },
@@ -49,7 +49,7 @@ const destinations = [
         name: 'Pulau Komodo',
         location: 'East Nusa Tenggara',
         category: 'alam',
-        image: 'https://images.unsplash.com/photo-1559827260-dc66d52bef19?w=800',
+        image: 'https://plus.unsplash.com/premium_photo-1661876927993-bedb3ab87208?q=80&w=955',
         description: 'The natural habitat of Komodo dragons with incredible landscapes',
         popular: false
     },
@@ -58,7 +58,7 @@ const destinations = [
         name: 'Kampung Budaya Betawi',
         location: 'Jakarta',
         category: 'budaya',
-        image: 'https://images.unsplash.com/photo-1559827260-dc66d52bef19?w=800',
+        image: 'https://nativeindonesia.com/foto/2021/09/situ-babakan.jpg',
         description: 'Experience Betawi culture with various traditional performances',
         popular: false
     },
@@ -67,7 +67,7 @@ const destinations = [
         name: 'Pasar Gede Solo',
         location: 'Surakarta, Central Java',
         category: 'kuliner',
-        image: 'https://images.unsplash.com/photo-1559827260-dc66d52bef19?w=800',
+        image: 'https://www.jogjasuper.co.id/wp-content/uploads/2024/03/lokasi-Pasar-Gede-Hardjonagoro.png',
         description: 'Traditional market offering various authentic Solo culinary delights',
         popular: false
     },
@@ -76,7 +76,7 @@ const destinations = [
         name: 'Danau Toba',
         location: 'North Sumatra',
         category: 'alam',
-        image: 'https://images.unsplash.com/photo-1559827260-dc66d52bef19?w=800',
+        image: 'https://images.unsplash.com/photo-1623598122059-9b5ef17619c8?q=80&w=870',
         description: 'The largest volcanic lake in Indonesia with breathtaking scenery',
         popular: false
     },
@@ -85,7 +85,7 @@ const destinations = [
         name: 'Warung Mak Beng',
         location: 'Sanur, Bali',
         category: 'kuliner',
-        image: 'https://images.unsplash.com/photo-1559827260-dc66d52bef19?w=800',
+        image: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/11/4f/ea/b6/view-from-outside.jpg?w=900',
         description: 'Legendary eatery serving signature Balinese grilled fish',
         popular: false
     },
@@ -94,7 +94,7 @@ const destinations = [
         name: 'Museum Nasional',
         location: 'Jakarta',
         category: 'budaya',
-        image: 'https://images.unsplash.com/photo-1559827260-dc66d52bef19?w=800',
+        image: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/17/6e/01/bb/the-building.jpg?w=900',
         description: 'The largest museum in Indonesia with a rich collection of historical artifacts',
         popular: false
     },
@@ -103,7 +103,7 @@ const destinations = [
         name: 'Gudeg Jogja',
         location: 'Yogyakarta',
         category: 'kuliner',
-        image: 'https://images.unsplash.com/photo-1559827260-dc66d52bef19?w=800',
+        image: 'https://cdn-1.timesmedia.co.id/images/2022/12/21/gudeg-2.jpg',
         description: 'Iconic Yogyakarta dish that every visitor should try',
         popular: false
     }
